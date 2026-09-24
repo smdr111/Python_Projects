a8d43b562934329761aa2e58a4ba34b5d9c9b206
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-09-24 14:07:56 -0400

    Update main.py

--- Authenticatation/main.py
+++ Authenticatation/main.py
@@ -6,7 +6,7 @@ from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yaw6gd67902j7h&0$3bgsywB8M670gf2C!'
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
